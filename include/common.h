d506391c130e6456ae8d31f2b86318fbda833cec
Rapolas Daugelis <rapolui@gmail.com>
2024-04-06 13:45:55 +0300

    warning messages in common.h

--- include/common.h
+++ include/common.h
@@ -5,6 +5,8 @@
 #define ERR(x, ...) fprintf(stderr, "App-ERROR: "x"\n", ##__VA_ARGS__)
 #define GL_ERR(x, ...) fprintf(stderr, "OpenGL-ERROR: "x"\n", ##__VA_ARGS__)
 
+#define WARN(x, ...) fprintf(stdout, "App-WARNING: "x"\n", ##__VA_ARGS__)
+
 #define GCC_IGN_RES(x) _Pragma("GCC diagnostic ignored \"-Wunused-result\"") \
     x; \
     _Pragma("GCC diagnostic pop")
